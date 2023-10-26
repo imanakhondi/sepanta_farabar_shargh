6def5f3bad858f6a3b33b4734d956edf51d2de6c
imankd <akhondiiman@gmail.com>
2023-10-26 19:08:32 +0330

    fixed bug - carIntroductionSecondStep

--- src/resources/js/resources/Pages/Introduction/Cars/EditCarIntroduction/index.jsx
+++ src/resources/js/resources/Pages/Introduction/Cars/EditCarIntroduction/index.jsx
@@ -15,26 +15,11 @@ import { editCarIntroductionPage } from "../../../../constants/strings/fa";
 
 import { CarIntroduction } from "../../../../http/entities/CarIntroduction";
 
-const driverInfoOptions = [
-    { id: 1, name: "آوا شریعت-0740004931-09356451716" },
-    { id: 2, name: "کسرا آخوندی-0744932536-09153297600" },
-    { id: 3, name: "دریا آخوندی-0742536985-09151264136" },
-    { id: 4, name: "ایلیا شریعت-0741523691-09151111213" },
-];
-const carInfoOptions = [
-    { id: 1, name: "21ع426-12 / 21u222-36" },
-    { id: 2, name: "73ع865-13" },
-    { id: 3, name: "99ع526-15" },
-    { id: 4, name: "32ع426-15" },
-];
-const tankInfoOptions = [
-    { id: 1, name: "123" },
-    { id: 2, name: "956" },
-    { id: 3, name: "241" },
-    { id: 4, name: "756" },
-];
 
 const initialValues = {
+    driverInfoOptions: [],
+    carInfoOptions: [],
+    tankInfoOptions: [],
     driverInfo: "",
     carInfo: "",
     tankInfo: "",
@@ -58,32 +43,82 @@ const EditCarIntroduction = () => {
     const dispatch = useDispatch();
     const [loading, setLoading] = useState(false);
 
-    const getCarIntroduction = async () => {
-        setLoading(true);
-        const result = await carIntroduction.getCarIntroduction(carId);
-        if (result === null) {
-            dispatch(
-                setMessageAction(
-                    carIntroduction.errorMessage,
-                    carIntroduction.errorCode
-                )
-            );
-            setLoading(false);
-
-            return;
-        }
-        setLoading(false);
-        setFormValues(result.item);
-    };
+    
 
     useEffect(() => {
         dispatch(clearMessageAction());
-        // getCarIntroduction();
-        setFormValues({
-            driverInfo: "آوا شریعت-0740004931-09356451716",
-            carInfo: "21ع426-12 / 21u222-36",
-            tankInfo: "123",
-        });
+
+        const getAllProps = async () => {
+            setLoading(true);
+            const result = await carIntroduction.getAddCarsIntroductionProps(
+                carId
+            );
+
+            console.log("naghi",result);
+            if (result === null) {
+                dispatch(
+                    setMessageAction(
+                        carIntroduction.errorMessage,
+                        carIntroduction.errorCode
+                    )
+                );
+                setLoading(false);
+
+                return;
+            }
+            setTimeout(() => setLoading(false), 200);
+
+            const drivers = result.drivers?.map((driver) => ({
+                id: driver.id,
+                name: `${driver.name} - ${driver.family} - ${driver.nationalNo} - ${driver.mobile}`,
+            }));
+
+            const trucks = result.trucks?.map((truck) => ({
+                id: truck.id,
+                name: `${truck.irNo} - ${truck.transitNo} `,
+            }));
+
+            const tanks = result.tanks?.map((tank) => ({
+                id: tank.id,
+                name: `${tank.tankNo} `,
+            }));
+
+            formik.setFieldValue("driverInfoOptions", drivers);
+            formik.setFieldValue("carInfoOptions", trucks);
+            formik.setFieldValue("tankInfoOptions", tanks);
+                        
+        };
+        // getAllProps();
+    }, []);
+
+    useEffect(() => {
+        dispatch(clearMessageAction());
+        const getCarIntroduction = async () => {
+            setLoading(true);
+            const result = await carIntroduction.getCarIntroduction(carId);
+    console.log("iman",result);
+    
+            if (result === null) {
+                dispatch(
+                    setMessageAction(
+                        carIntroduction.errorMessage,
+                        carIntroduction.errorCode
+                    )
+                );
+                setLoading(false);
+    
+                return;
+            }
+            setLoading(false);
+            setFormValues(result.item);
+           
+        };
+        getCarIntroduction();
+        // setFormValues({
+        //     driverInfo: "آوا شریعت-0740004931-09356451716",
+        //     carInfo: "21ع426-12 / 21u222-36",
+        //     tankInfo: "123",
+        // });
     }, []);
 
     const onSubmit = async (values) => {
@@ -130,21 +165,21 @@ const EditCarIntroduction = () => {
                 name="driverInfo"
                 formik={formik}
                 pageString={editCarIntroductionPage}
-                selectOptions={driverInfoOptions}
+                selectOptions={formik.values.driverInfoOptions}
             />
             <FormikControl
                 control="searchableDropdown"
                 name="carInfo"
                 formik={formik}
                 pageString={editCarIntroductionPage}
-                selectOptions={carInfoOptions}
+                selectOptions={formik.values.carInfoOptions}
             />
             <FormikControl
                 control="searchableDropdown"
                 name="tankInfo"
                 formik={formik}
                 pageString={editCarIntroductionPage}
-                selectOptions={tankInfoOptions}
+                selectOptions={formik.values.tankInfoOptions}
             />
         </FormikForm>
     );
